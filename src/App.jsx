bbea15266fde16ebb221b60fab4a13fa4d267fe5
hervekwizera <hervekwizera63@gmail.com>
2024-08-28 07:23:05 +1300

    addition of complete button

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,24 @@ function App() {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
+      completed: false,
     }
     setTodoList([...todoList, task]);
   }
-  const deleteText = (id) => {
+  const deleteTask = (id) => {
     setTodoList(todoList.filter((task) => task.id !== id))
+  }
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true }
+        } else {
+          return task;
+        }
+      }
+      )
+    )
   }
 
   return (
@@ -27,7 +40,12 @@ function App() {
       </div>
       <div className='list'>
         {todoList.map((task) =>
-          <Task taskName={task.taskName} id={task.id} deleteText={deleteText} />
+          <Task taskName={task.taskName}
+            id={task.id}
+            completed ={task.completed}
+            deleteTask={deleteTask}
+            completeTask={completeTask}
+          />
         )}
       </div>
     </div>
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,8 +1,12 @@
 export const Task =(props)=>{
     return(
-        <div>
+        <div
+         className="task"
+         style={{backgroundColor:props.completed?"green":"white"}}
+        >
             <h1>{props.taskName}</h1>
-            <button onClick={() => props.deleteText(props.id)}>x</button>
+            <button onClick={()=>props.completeTask(props.id)}>completed</button>
+            <button  onClick={() => props.deleteTask(props.id)}>x</button>
           </div>
     )
 }
